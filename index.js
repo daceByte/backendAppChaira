b442a79f2413e05f2025a6ec4c079ceb86240a86
Carlos Mauricio Roa <57810969+Elkarloz@users.noreply.github.com>
2023-02-12 17:35:55 -0500

    api v1

--- index.js
+++ index.js
@@ -3,7 +3,19 @@ const puppeteer = require("puppeteer");
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
+
   await page.goto("https://chaira.uniamazonia.edu.co/Chaira/Logon.aspx");
-  // other actions...
-  //await browser.close();
+  await page.setViewport({ width: 800, height: 600 });
+  await page.type("#txt_usuario", "di.cardenas");
+  await page.type("#txt_password", "dace2003");
+  const btn_ingresar = await page.waitForSelector("#btn_ingresar");
+  await btn_ingresar.click();
+
+  const textSelector = await page.waitForSelector("text/Inicio");
+
+  await textSelector.click();
+
+  /* await btn_horario.click(); */
 })();
+
+/* ext-gen339 */
